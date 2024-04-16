b8f5fdd4ee3da796ad88430f23cf7fb1ef7adebf
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-04-16 20:41:02 -0300

    Stateful Widget, setState

---
+++ android/aulas/stateful.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp(nome: 'Nicolas',));
+}
+
+
+class MyApp extends StatefulWidget {
+
+  final String nome;
+
+  const MyApp({super.key, this.nome = ''});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int salario = 2000;
+
+  void aumentarSalario(int valor){
+    setState(() {
+      this.salario += valor;
+    });
+  }
+
+  void diminuirSalario(int valor){
+    setState(() {
+      this.salario -= valor;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: (){
+          print("Clicou");
+          diminuirSalario(100);
+          },
+        
+        child: Text(
+          "O salario de ${widget.nome} é $salario",
+          textDirection: TextDirection.ltr,),
+      ),
+
+    );
+  }
+}
