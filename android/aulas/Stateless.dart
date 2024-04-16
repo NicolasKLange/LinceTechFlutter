fbddbf5b5adabe637011ce808327dc62c0bff6ff
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-04-16 20:06:09 -0300

    Aulas de Flutter

---
+++ android/aulas/Stateless.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  int valor = 10;
+
+  runApp(MyApp(titulo: "Cadastramento", valor: valor));
+}
+
+class MyApp extends StatelessWidget{
+  final String titulo;
+  final int valor;
+  const MyApp({Key? key, this.titulo = '', this.valor = 0}) : super(key: key);
+
+  @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(this.titulo),
+          ),
+          body: Center(
+            child: Text("Cadastro", style: TextStyle(fontSize: 40, color: Colors.brown),
+            ),
+          ),
+        ),
+      );
+    }
+}
