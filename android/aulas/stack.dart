659dd5ea3b7663ddd374ac11ed31822bd6b0a303
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-04-18 15:04:38 -0300

    Stack

---
+++ android/aulas/stack.dart
@@ -0,0 +1,59 @@
+///STACK > WIDGET PARA IR EMPILHANDO
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Stack App')
+        ),
+        body: Stack(
+          //alignment: AlignmentDirectional.center, > alinha os container ao centro
+          //fit: StackFit.expand, > espande os containers
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.lightBlue,
+            ),
+            //Posiciona o container
+            Positioned(
+              bottom: 10,  //se desloca 10px em relacao a base
+              right: 10, //se desloca 10px em relacao a esquerda
+              child: Container(
+                width: 150,
+                height: 150,
+                color: Colors.red,
+              ),
+            ),
+            Positioned(
+              top: 10, //se desloca 10px em relacao ao topo
+              left: 10, //se desloca 10px em relacao a esquerda
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.indigoAccent,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
