47df44469d4e13847480bdc3a9e6f74ef655c2f8
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-05-04 19:34:41 -0300

    Atividade > Finalizada

--- android/apf6_funcoes/ap1.dart
+++ android/apf6_funcoes/ap1.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
-
 void main() {
   runApp(MyApp());
 }
@@ -19,54 +17,49 @@ enum TipoSanguineo {
 }
 
 class Pessoa {
-  const Pessoa({
+  final String nome;
+  final String email;
+  final String telefone;
+  final String github;
+  final TipoSanguineo tipoSanguineo;
+
+  Pessoa({
     required this.nome,
     required this.email,
     required this.telefone,
     required this.github,
     required this.tipoSanguineo,
   });
-
-  final String nome;
-  final String email;
-  final String telefone;
-  final String github;
-  final TipoSanguineo tipoSanguineo;
 }
 
-class EstadoListaDePessoas with ChangeNotifier {
-  final _listaDePessoas = <Pessoa>[];
+class ListaDePessoas extends ChangeNotifier {
+  final List<Pessoa> _pessoas = [];
 
-  List<Pessoa> get pessoas => List.unmodifiable(_listaDePessoas);
+  List<Pessoa> get pessoas => List.unmodifiable(_pessoas);
 
-  void incluir(Pessoa pessoa) {
-    _listaDePessoas.add(pessoa);
+  void adicionar(Pessoa pessoa) {
+    _pessoas.add(pessoa);
     notifyListeners();
   }
 
-  void excluir(Pessoa pessoa) {
-    _listaDePessoas.remove(pessoa);
+  void remover(Pessoa pessoa) {
+    _pessoas.remove(pessoa);
     notifyListeners();
   }
-
-//
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => EstadoListaDePessoas(),
+      create: (_) => ListaDePessoas(),
       child: MaterialApp(
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: darkBlue,
-        ),
-        debugShowCheckedModeBanner: false,
+        title: 'Cadastro de Pessoas',
         initialRoute: '/',
         routes: {
           '/': (context) => TelaInicial(),
           '/listagem': (context) => TelaListagem(),
-          '/inclusao_edicao': (context) => TelaInclusaoEdicao(),
+          '/cadastro': (context) => TelaCadastro(),
         },
       ),
     );
@@ -77,22 +70,37 @@ class TelaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Tela Inicial')),
+      appBar: AppBar(
+        title: Text(
+          'Tela Inicial',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/listagem');
-              },
-              child: Text('Ver Lista de Pessoas'),
+              onPressed: () => Navigator.pushNamed(context, '/listagem'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue, // Cor de fundo azul
+              ),
+              child: Text(
+                'Ver Lista de Pessoas',
+                style: TextStyle(fontSize: 20, color: Colors.white), // Texto branco
+              ),
             ),
+            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/inclusao_edicao');
-              },
-              child: Text('Incluir Nova Pessoa'),
+              onPressed: () => Navigator.pushNamed(context, '/cadastro'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: Text(
+                'Cadastrar Nova Pessoa',
+                style: TextStyle(fontSize: 20, color: Colors.white), // Texto branco
+              ),
             ),
           ],
         ),
@@ -101,122 +109,347 @@ class TelaInicial extends StatelessWidget {
   }
 }
 
+
 class TelaListagem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final listaDePessoas = Provider.of<EstadoListaDePessoas>(context);
+    final listaDePessoas = Provider.of<ListaDePessoas>(context);
 
     return Scaffold(
-      appBar: AppBar(title: Text('Listagem de Pessoas')),
-      body: Column(
-        children: [
-          TextField(
-            onChanged: (value) {
-              // Implementar a lógica de filtragem
+      appBar: AppBar(
+        title: Text(
+          'Lista de Pessoas',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+        itemCount: listaDePessoas.pessoas.length,
+        itemBuilder: (context, index) {
+          final pessoa = listaDePessoas.pessoas[index];
+          Color cor;
+          switch (pessoa.tipoSanguineo) {
+            case TipoSanguineo.aPositivo:
+              cor = Colors.blue;
+              break;
+            case TipoSanguineo.aNegativo:
+              cor = Colors.red;
+              break;
+            case TipoSanguineo.bPositivo:
+              cor = Colors.purple;
+              break;
+            case TipoSanguineo.bNegativo:
+              cor = Colors.orange;
+              break;
+            case TipoSanguineo.oPositivo:
+              cor = Colors.green;
+              break;
+            case TipoSanguineo.oNegativo:
+              cor = Colors.yellow;
+              break;
+            case TipoSanguineo.abPositivo:
+              cor = Colors.cyan;
+              break;
+            case TipoSanguineo.abNegativo:
+              cor = Colors.white24;
+              break;
+          }
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => TelaDetalhesPessoa(pessoa: pessoa),
+                ),
+              );
             },
-            decoration: InputDecoration(
-              hintText: 'Filtrar por nome...',
-              prefixIcon: Icon(Icons.search),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: cor,
+                child: Text(pessoa.nome[0]),
+              ),
+              title: Text(pessoa.nome),
+              subtitle: Text(pessoa.email),
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () => listaDePessoas.remover(pessoa),
+              ),
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: listaDePessoas.pessoas.length,
-              itemBuilder: (context, index) {
-                final pessoa = listaDePessoas.pessoas[index];
-                Color cor;
-                switch (pessoa.tipoSanguineo) {
-                  case TipoSanguineo.aPositivo:
-                    cor = Colors.blue;
-                    break;
-                  case TipoSanguineo.aNegativo:
-                    cor = Colors.red;
-                    break;
-                  case TipoSanguineo.bPositivo:
-                    cor = Colors.purple;
-                    break;
-                  case TipoSanguineo.bNegativo:
-                    cor = Colors.orange;
-                    break;
-                  case TipoSanguineo.oPositivo:
-                    cor = Colors.green;
-                    break;
-                  case TipoSanguineo.oNegativo:
-                    cor = Colors.yellow;
-                    break;
-                  case TipoSanguineo.abPositivo:
-                    cor = Colors.cyan;
-                    break;
-                  case TipoSanguineo.abNegativo:
-                    cor = Colors.white;
-                    break;
-                }
-                return ListTile(
-                  title: Text(pessoa.nome),
-                  subtitle: Text(pessoa.email),
-                  leading: CircleAvatar(
-                    backgroundColor: cor,
-                    child: Text(pessoa.nome[0]),
-                  ),
-                  onTap: () {
-                    // Implementar ação de visualizar/editar pessoa
-                  },
-                );
-              },
-            ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
 }
 
-class TelaInclusaoEdicao extends StatelessWidget {
+class TelaCadastro extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final _nomeController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _telefoneController = TextEditingController();
+  final _githubController = TextEditingController();
+  TipoSanguineo? _tipoSanguineoSelecionado;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Incluir/Editar Pessoa')),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16),
+      appBar: AppBar(
+        title: Text(
+          'Cadastro de Pessoa',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Form(
-          // Implementar formulário de inclusão/editação aqui
+          key: _formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               TextFormField(
-                decoration: InputDecoration(labelText: 'Nome Completo'),
+                controller: _nomeController,
+                decoration: InputDecoration(
+                  label: Text(
+                    'Nome Completo',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                validator: (value) =>
+                value!.isEmpty ? 'Por favor, insira o nome.' : null,
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'E-mail'),
+                controller: _emailController,
+                decoration: InputDecoration(
+                  label: Text(
+                    'E-mail',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor, insira o e-mail.';
+                  } else if (!value.contains('@')) {
+                    return 'E-mail inválido.';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Telefone'),
+                controller: _telefoneController,
+                decoration: InputDecoration(
+                  label: Text(
+                    'Telefone',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                validator: (value) =>
+                value!.isEmpty ? 'Por favor, insira o telefone.' : null,
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Link do GitHub'),
+                controller: _githubController,
+                decoration: InputDecoration(
+                  label: Text(
+                    'Link do GitHub',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                // GitHub não é obrigatório, portanto, não adicionaremos validação
               ),
               DropdownButtonFormField<TipoSanguineo>(
-                decoration: InputDecoration(labelText: 'Tipo Sanguíneo'),
+                value: _tipoSanguineoSelecionado,
+                onChanged: (value) {
+                  _tipoSanguineoSelecionado = value;
+                },
                 items: TipoSanguineo.values
                     .map((tipo) => DropdownMenuItem(
-                  child: Text(tipo.toString().split('.').last),
                   value: tipo,
+                  child: Text(tipo.toString().split('.').last),
                 ))
                     .toList(),
-                onChanged: (value) {
-                  // Implementar lógica de seleção do tipo sanguíneo
-                },
+                decoration: InputDecoration(
+                  label: Text(
+                    'Tipo Sanguíneo',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  // Implementar ação de salvar pessoa
-                },
-                child: Text('Salvar'),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      final pessoa = Pessoa(
+                        nome: _nomeController.text,
+                        email: _emailController.text,
+                        telefone: _telefoneController.text,
+                        github: _githubController.text,
+                        tipoSanguineo: _tipoSanguineoSelecionado!,
+                      );
+                      Provider.of<ListaDePessoas>(context, listen: false)
+                          .adicionar(pessoa);
+                      Navigator.pop(context);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue, // Cor de fundo azul
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Salvar',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white, // Texto branco
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class TelaDetalhesPessoa extends StatelessWidget {
+  final Pessoa pessoa;
+
+  const TelaDetalhesPessoa({required this.pessoa});
+
+  @override
+  Widget build(BuildContext context) {
+    Color cor;
+    switch (pessoa.tipoSanguineo) {
+      case TipoSanguineo.aPositivo:
+        cor = Colors.blue;
+        break;
+      case TipoSanguineo.aNegativo:
+        cor = Colors.red;
+        break;
+      case TipoSanguineo.bPositivo:
+        cor = Colors.purple;
+        break;
+      case TipoSanguineo.bNegativo:
+        cor = Colors.orange;
+        break;
+      case TipoSanguineo.oPositivo:
+        cor = Colors.green;
+        break;
+      case TipoSanguineo.oNegativo:
+        cor = Colors.yellow;
+        break;
+      case TipoSanguineo.abPositivo:
+        cor = Colors.cyan;
+        break;
+      case TipoSanguineo.abNegativo:
+        cor = Colors.white24;
+        break;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Informações pesoais",
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: cor,
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  _obterTipoSanguineoAbreviado(pessoa.tipoSanguineo),
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Nome: ${pessoa.nome}',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              'E-mail: ${pessoa.email}',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Telefone: ${pessoa.telefone}',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Link do GitHub: ${pessoa.github}',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Tipo Sanguíneo: ${_obterTipoSanguineo(pessoa.tipoSanguineo)}',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _obterTipoSanguineo(TipoSanguineo tipoSanguineo) {
+    switch (tipoSanguineo) {
+      case TipoSanguineo.aPositivo:
+        return 'A+';
+      case TipoSanguineo.aNegativo:
+        return 'A-';
+      case TipoSanguineo.bPositivo:
+        return 'B+';
+      case TipoSanguineo.bNegativo:
+        return 'B-';
+      case TipoSanguineo.oPositivo:
+        return 'O+';
+      case TipoSanguineo.oNegativo:
+        return 'O-';
+      case TipoSanguineo.abPositivo:
+        return 'AB+';
+      case TipoSanguineo.abNegativo:
+        return 'AB-';
+    }
+  }
+
+  String _obterTipoSanguineoAbreviado(TipoSanguineo tipoSanguineo) {
+    switch (tipoSanguineo) {
+      case TipoSanguineo.aPositivo:
+        return 'A+';
+      case TipoSanguineo.aNegativo:
+        return 'A-';
+      case TipoSanguineo.bPositivo:
+        return 'B+';
+      case TipoSanguineo.bNegativo:
+        return 'B-';
+      case TipoSanguineo.oPositivo:
+        return 'O+';
+      case TipoSanguineo.oNegativo:
+        return 'O-';
+      case TipoSanguineo.abPositivo:
+        return 'AB+';
+      case TipoSanguineo.abNegativo:
+        return 'AB-';
+    }
   }
 }
